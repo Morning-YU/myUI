172d02b54ffa07b094a3f55018b7571e27c3e0ad
Morning-YU <44916658+Morning-YU@users.noreply.github.com>
2022-06-17 14:20:46 +0800

    Add files via upload

---
+++ src/views/swiperView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="block">
+    <my-swiper class="swiperBlock" :imgList="imgList" :initialInterval = 3000> </my-swiper>
+  </div>
+</template>
+
+<script>
+import mySwiper from "../components/mySwiper.vue";
+
+export default {
+  name: "swiperView",
+  components: {
+    mySwiper,
+  },
+  data() {
+    return {
+      imgList: [
+        require("../assets/img/1.jpg"),
+        require("../assets/img/2.jpg"),
+        require("../assets/img/3.jpg"),
+        require("../assets/img/4.jpg"),
+        // require("../assets/img/5.jpg"),
+        // require("../assets/img/6.jpg"),
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* .block{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+} */
+</style>
